c467d750e0372c1cd1c4875286506a72aca121bf
Andy Palmbach <andyp@stellarbluetechnologies.com>
2021-04-14 03:14:42 -0500

    Created BL FEV1Manager and BL Test utFEV1Manager

---
+++ LungTracking/LungTracking.BL.Test/utFEV1Manager.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using LungTracking.BL;
+using LungTracking.BL.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LungTracking.BL.Test
+{
+    [TestClass]
+    public class utFEV1Manager
+    {
+        DateTime timeOfDay = DateTime.Parse("2021-04-14 11:00:00");
+        Guid patientId = Guid.Parse("dbb77cb3-c7b1-42d5-ae1e-0e2c66fc0c4c");
+
+        [TestMethod]
+        public void LoadTest()
+        {
+            List<FEV1> fev1s = new List<FEV1>();
+            fev1s = FEV1Manager.Load();
+            int expected = 300;
+            Assert.AreEqual(expected, fev1s.Count);
+        }
+
+        [TestMethod]
+        public void LoadByPatientIdTest()
+        {
+            List<FEV1> fev1s = new List<FEV1>();
+            fev1s = FEV1Manager.LoadByPatientId(patientId);
+            Assert.IsNotNull(fev1s);
+        }
+
+        [TestMethod]
+        public void InsertTest()
+        {
+            FEV1 fev1 = new FEV1();
+            fev1.FEV1Number = 3.3M;
+            fev1.BeginningEnd = true;
+            fev1.TimeOfDay = timeOfDay;
+            fev1.PatientId = patientId;
+
+            int result = FEV1Manager.Insert(fev1);
+            Assert.IsTrue(result > 0);
+
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            List<FEV1> fev1s = FEV1Manager.Load();
+
+            FEV1 fev1 = fev1s.Where(a => a.TimeOfDay == timeOfDay && a.PatientId == patientId).FirstOrDefault();
+
+            fev1.FEV1Number = 4.1M;
+
+            FEV1Manager.Update(fev1);
+
+            FEV1 updatedfev1 = fev1s.FirstOrDefault(a => a.TimeOfDay == timeOfDay && a.PatientId == patientId);
+
+            Assert.AreEqual(fev1.FEV1Number, updatedfev1.FEV1Number);
+        }
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            List<FEV1> fev1s = FEV1Manager.Load();
+            FEV1 fev1 = fev1s.Where(a => a.TimeOfDay == timeOfDay && a.PatientId == patientId).FirstOrDefault();
+
+            int results = FEV1Manager.Delete(fev1.Id);
+            Assert.IsTrue(results > 0);
+        }
+    }
+}
---
+++ LungTracking/LungTracking.BL/FEV1Manager.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LungTracking.BL.Models;
+using LungTracking.PL;
+
+namespace LungTracking.BL
+{
+    public static class FEV1Manager
+    {
+        public static List<FEV1> Load()
+        {
+            using (LungTrackingEntities dc = new LungTrackingEntities())
+            {
+                List<FEV1> fev1 = new List<FEV1>();
+
+                dc.tblFev1s
+                    .ToList()
+                    .ForEach(u => fev1.Add(new FEV1
+                    {
+                        Id = u.Id,
+                        FEV1Number = u.Fev1number,
+                        BeginningEnd = u.BeginningEnd,
+                        TimeOfDay = u.TimeOfDay,
+                        PatientId = u.PatientId
+                    }));
+                return fev1;
+            }
+        }
+        public static int Insert(int fev1Number, bool beginningEnd, DateTime timeOfDay, Guid patientId)
+        {
+            try
+            {
+                using (LungTrackingEntities dc = new LungTrackingEntities())
+                {
+                    tblFev1 newFEV1 = new tblFev1
+                    {
+                        Id = Guid.NewGuid(),
+                        Fev1number = fev1Number,
+                        BeginningEnd = beginningEnd,
+                        TimeOfDay = timeOfDay,
+                        PatientId = patientId
+                    };
+                    dc.tblFev1s.Add(newFEV1);
+                    return dc.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public static int Insert(FEV1 fev1)
+        {
+            try
+            {
+                using (LungTrackingEntities dc = new LungTrackingEntities())
+                {
+                    tblFev1 newFEV1 = new tblFev1
+                    {
+                        Id = Guid.NewGuid(),
+                        Fev1number = fev1.FEV1Number,
+                        BeginningEnd = fev1.BeginningEnd,
+                        TimeOfDay = fev1.TimeOfDay,
+                        PatientId = fev1.PatientId
+                    };
+                    dc.tblFev1s.Add(newFEV1);
+                    return dc.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public static int Update(Guid id, decimal fev1Number, bool beginningEnd, DateTime timeOfDay, Guid patientId)
+        {
+            try
+            {
+                using (LungTrackingEntities dc = new LungTrackingEntities())
+                {
+                    tblFev1 updaterow = (from dt in dc.tblFev1s where dt.Id == id select dt).FirstOrDefault();
+                    updaterow.Fev1number = fev1Number;
+                    updaterow.BeginningEnd = beginningEnd;
+                    updaterow.TimeOfDay = timeOfDay;
+                    updaterow.PatientId = patientId;
+                    return dc.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public static int Update(FEV1 fev1)
+        {
+            return Update(fev1.Id, fev1.FEV1Number, fev1.BeginningEnd, fev1.TimeOfDay, fev1.PatientId); ;
+        }
+
+        public static List<FEV1> LoadByPatientId(Guid patientId)
+        {
+            try
+            {
+                using (LungTrackingEntities dc = new LungTrackingEntities())
+                {
+                    List<FEV1> fev1 = new List<FEV1>();
+
+                    var results = (from bs in dc.tblFev1s
+                                   where bs.PatientId == patientId
+                                   select new
+                                   {
+                                       bs.Id,
+                                       bs.Fev1number,
+                                       bs.BeginningEnd,
+                                       bs.TimeOfDay,
+                                       bs.PatientId
+                                   }).ToList();
+
+                    results.ForEach(r => fev1.Add(new FEV1
+                    {
+                        Id = r.Id,
+                        FEV1Number = r.Fev1number,
+                        BeginningEnd = r.BeginningEnd,
+                        TimeOfDay = r.TimeOfDay,
+                        PatientId = r.PatientId
+                    }));
+
+                    return fev1;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public static int Delete(Guid id)
+        {
+            try
+            {
+                using (LungTrackingEntities dc = new LungTrackingEntities())
+                {
+                    tblFev1 deleterow = (from dt in dc.tblFev1s where dt.Id == id select dt).FirstOrDefault();
+                    dc.tblFev1s.Remove(deleterow);
+                    return dc.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
