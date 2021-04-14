55c0a846aeda34e1a03acf4801caa5a9d2286652
Andy Palmbach <andyp@stellarbluetechnologies.com>
2021-04-14 02:12:59 -0500

    Created BL MedicationTrackingManager and BL Test utMedicationTrackingManager

---
+++ LungTracking/LungTracking.BL.Test/utMedicationTrackingManager.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using LungTracking.BL;
+using LungTracking.BL.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LungTracking.BL.Test
+{
+    [TestClass]
+    public class utMedicationTrackingManager
+    {
+        Guid medicationId = Guid.Parse("aa596935-5d6f-4a1d-940e-3c3afb6fd9c7");
+        Guid patientId = Guid.Parse("dbb77cb3-c7b1-42d5-ae1e-0e2c66fc0c4c");
+
+        [TestMethod]
+        public void LoadTest()
+        {
+            List<MedicationTracking> mts = new List<MedicationTracking>();
+            mts = MedicationTrackingManager.Load();
+            int expected = 300;
+            Assert.AreEqual(expected, mts.Count);
+        }
+
+        [TestMethod]
+        public void LoadByPatientIdTest()
+        {
+            List<MedicationTracking> mts = new List<MedicationTracking>();
+            mts = MedicationTrackingManager.LoadByPatientId(patientId);
+            Assert.IsNotNull(mts);
+        }
+
+        [TestMethod]
+        public void InsertTest()
+        {
+            MedicationTracking mt = new MedicationTracking();
+            mt.PillTakenTime = DateTime.Parse("2021-04-14 11:00:00");
+            mt.MedicationId = medicationId;
+            mt.PatientId = patientId;
+
+            int result = MedicationTrackingManager.Insert(mt);
+            Assert.IsTrue(result > 0);
+
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            List<MedicationTracking> mts = MedicationTrackingManager.Load();
+
+            MedicationTracking mt = mts.Where(a => a.MedicationId == medicationId && a.PatientId == patientId).FirstOrDefault();
+
+            mt.PillTakenTime = DateTime.Parse("2021-04-14 12:00:00");
+
+            MedicationTrackingManager.Update(mt);
+
+            MedicationTracking updatedmt = mts.FirstOrDefault(a => a.MedicationId == medicationId && a.PatientId == patientId);
+
+            Assert.AreEqual(mt.PillTakenTime, updatedmt.PillTakenTime);
+        }
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            List<MedicationTracking> mts = MedicationTrackingManager.Load();
+            MedicationTracking mt = mts.Where(a => a.MedicationId == medicationId && a.PatientId == patientId).FirstOrDefault();
+
+            int results = MedicationTrackingManager.Delete(mt.Id);
+            Assert.IsTrue(results > 0);
+        }
+    }
+}
---
+++ LungTracking/LungTracking.BL/MedicationTrackingManager.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LungTracking.BL.Models;
+using LungTracking.PL;
+
+namespace LungTracking.BL
+{
+    public static class MedicationTrackingManager
+    {
+        public static List<MedicationTracking> Load()
+        {
+            using (LungTrackingEntities dc = new LungTrackingEntities())
+            {
+                List<MedicationTracking> medTracking = new List<MedicationTracking>();
+
+                dc.tblMedicationTrackings
+                    .ToList()
+                    .ForEach(u => medTracking.Add(new MedicationTracking
+                    {
+                        Id = u.Id,
+                        PillTakenTime = u.PillTakenTime,
+                        MedicationId = u.MedicationId,
+                        PatientId = u.PatientId
+                    }));
+                return medTracking;
+            }
+        }
+        public static int Insert(DateTime pillTakenTime, Guid medId, Guid patientId)
+        {
+            try
+            {
+                using (LungTrackingEntities dc = new LungTrackingEntities())
+                {
+                    tblMedicationTracking newMedTracking = new tblMedicationTracking
+                    {
+                        Id = Guid.NewGuid(),
+                        PillTakenTime = pillTakenTime,
+                        MedicationId = medId,
+                        PatientId = patientId
+                    };
+                    dc.tblMedicationTrackings.Add(newMedTracking);
+                    return dc.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public static int Insert(MedicationTracking medTracking)
+        {
+            try
+            {
+                using (LungTrackingEntities dc = new LungTrackingEntities())
+                {
+                    tblMedicationTracking newMedTracking = new tblMedicationTracking
+                    {
+                        Id = Guid.NewGuid(),
+                        PillTakenTime = medTracking.PillTakenTime,
+                        MedicationId = medTracking.MedicationId,
+                        PatientId = medTracking.PatientId
+                    };
+                    dc.tblMedicationTrackings.Add(newMedTracking);
+                    return dc.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public static int Update(Guid id, DateTime pillTakenTime, Guid medId, Guid patientId)
+        {
+            try
+            {
+                using (LungTrackingEntities dc = new LungTrackingEntities())
+                {
+                    tblMedicationTracking updaterow = (from dt in dc.tblMedicationTrackings where dt.Id == id select dt).FirstOrDefault();
+                    updaterow.PillTakenTime = pillTakenTime;
+                    updaterow.MedicationId = medId;
+                    updaterow.PatientId = patientId;
+                    return dc.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public static int Update(MedicationTracking medTracking)
+        {
+            return Update(medTracking.Id, medTracking.PillTakenTime, medTracking.MedicationId, medTracking.PatientId);
+        }
+
+        public static List<MedicationTracking> LoadByPatientId(Guid patientId)
+        {
+            try
+            {
+                using (LungTrackingEntities dc = new LungTrackingEntities())
+                {
+                    List<MedicationTracking> medTracking = new List<MedicationTracking>();
+
+                    var results = (from mdt in dc.tblMedicationTrackings
+                                   where mdt.PatientId == patientId
+                                   select new
+                                   {
+                                       mdt.Id,
+                                       mdt.PillTakenTime,
+                                       mdt.MedicationId,
+                                       mdt.PatientId
+                                   }).ToList();
+
+                    results.ForEach(r => medTracking.Add(new MedicationTracking
+                    {
+                        Id = r.Id,
+                        PillTakenTime = r.PillTakenTime,
+                        MedicationId = r.MedicationId,
+                        PatientId = r.PatientId
+                    }));
+
+                    return medTracking;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public static int Delete(Guid id)
+        {
+            try
+            {
+                using (LungTrackingEntities dc = new LungTrackingEntities())
+                {
+                    tblMedicationTracking deleterow = (from dt in dc.tblMedicationTrackings where dt.Id == id select dt).FirstOrDefault();
+                    dc.tblMedicationTrackings.Remove(deleterow);
+                    return dc.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
