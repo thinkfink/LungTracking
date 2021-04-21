519dd8c40588c1c21ad0ffb2cfa652d42114e4e6
Tyler <tylerfink95@gmail.com>
2021-04-20 22:47:48 -0500

    WeightTrackerPage, BloodTrackerPage, VitalTrackerPage

--- LungTracking/LungTracking.Mobile/LungTracking.Mobile/Views/BloodTrackerPage.xaml.cs
+++ LungTracking/LungTracking.Mobile/LungTracking.Mobile/Views/BloodTrackerPage.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using LungTracking.Mobile.Models;
+using LungTracking.Mobile.ViewModels;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,9 +16,29 @@ namespace LungTracking.Mobile.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class BloodTrackerPage : ContentPage
     {
+        public Item Item { get; set; }
+
         public BloodTrackerPage()
         {
             InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+
+        private static HttpClient InitializeClient()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44364/");
+            return client;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            HttpClient client = InitializeClient();
+            BloodSugar bloodSugar = new Models.BloodSugar { BloodSugarNumber = Convert.ToInt32(txtBloodSugarNumber.Text), TimeOfDay = DateTime.Now, UnitsOfInsulinGiven = Convert.ToInt32(txtUnitsOfInsulinGiven.Text) };
+            string serializedObject = JsonConvert.SerializeObject(bloodSugar);
+            var content = new StringContent(serializedObject);
+            content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+            HttpResponseMessage response = client.PutAsync("BloodSugar/" + bloodSugar.Id, content).Result;
         }
     }
 }
--- LungTracking/LungTracking.Mobile/LungTracking.Mobile/Views/VitalTrackerPage.xaml.cs
+++ LungTracking/LungTracking.Mobile/LungTracking.Mobile/Views/VitalTrackerPage.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using LungTracking.Mobile.Models;
+using LungTracking.Mobile.ViewModels;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,9 +16,30 @@ namespace LungTracking.Mobile.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class VitalTrackerPage : ContentPage
     {
+        public Item Item { get; set; }
         public VitalTrackerPage()
         {
             InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+
+        private static HttpClient InitializeClient()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44364/");
+            return client;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            /*
+            //PEF, FEV1, Pulse, Temp
+            HttpClient client = InitializeClient();
+            Weight weight = new Models.Weight { WeightNumberInPounds = Convert.ToInt32(txtNumber.Text), TimeOfDay = DateTime.Now };
+            string serializedObject = JsonConvert.SerializeObject(weight);
+            var content = new StringContent(serializedObject);
+            content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+            HttpResponseMessage response = client.PutAsync("Weight/" + weight.Id, content).Result;*/
         }
     }
 }
