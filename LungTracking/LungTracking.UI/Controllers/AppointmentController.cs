147db22bda0af80c8f918e05963cae73277533cc
Jason Jeffers <120430452@fvtc.edu>
2021-05-03 22:17:24 -0500

    added navbar with drop down

---
+++ LungTracking/LungTracking.UI/Controllers/AppointmentController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using LungTracking.BL.Models;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace LungTracking.UI.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        // GET: AppointmentController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AppointmentController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AppointmentController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AppointmentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Appointment appointment)
+        {
+            try
+            {
+                HttpResponseMessage response;
+                string result;
+                dynamic items;
+
+                HttpClient client = InitializeClient();
+
+                response = client.GetAsync("Appointment/" + appointment.PatientId).Result;
+                result = response.Content.ReadAsStringAsync().Result;
+                items = (JArray)JsonConvert.DeserializeObject(result);
+                List<Appointment> appointments = items.ToObject<List<Appointment>>();
+
+                return View(nameof(Index), appointments);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        private static HttpClient InitializeClient()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44323/api/");
+            return client;
+        }
+
+        // GET: AppointmentController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AppointmentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AppointmentController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AppointmentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- LungTracking/LungTracking.UI/Startup.cs
+++ LungTracking/LungTracking.UI/Startup.cs
@@ -24,6 +24,8 @@ namespace LungTracking.UI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddHttpContextAccessor();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +41,7 @@ namespace LungTracking.UI
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseSession();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
