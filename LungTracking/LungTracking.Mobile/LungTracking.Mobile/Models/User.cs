60c7ad1f5d6792037e1700340c03901a24ba41cb
Andy Palmbach <andyp@stellarbluetechnologies.com>
2021-05-14 02:46:54 -0500

    Added Button_Clicked method to LoginPage on Mobile project

--- LungTracking/LungTracking.Mobile/LungTracking.Mobile/Models/User.cs
+++ LungTracking/LungTracking.Mobile/LungTracking.Mobile/Models/User.cs
@@ -13,5 +13,41 @@ namespace LungTracking.Mobile.Models
         public string Email { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastLogin { get; set; }
+
+        // Constructors
+        public User() { }
+
+        public User(string userName, string password)
+        {
+            // Logging In
+            Username = userName;
+            Password = password;
+            LastLogin = DateTime.Now;
+        }
+
+        public User(Guid id, string username, string password, int role, string email)
+        {
+            // Updating user
+            Id = id;
+            Username = username;
+            Password = password;
+            Role = role;
+            Email = email;
+        }
+
+        public User(string username, string password, int role, string email)
+        {
+            // Creating user
+            Username = username;
+            Password = password;
+            Role = role;
+            Email = email;
+            Created = DateTime.Now;
+            LastLogin = DateTime.Now;
+        }
+
+        // Variables for new password change
+        public string NewPassword { get; set; }
+        public string ConfirmPassword { get; set; }
     }
 }
--- LungTracking/LungTracking.Mobile/LungTracking.Mobile/Views/LoginPage.xaml.cs
+++ LungTracking/LungTracking.Mobile/LungTracking.Mobile/Views/LoginPage.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using LungTracking.Mobile.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,9 +20,22 @@ namespace LungTracking.Mobile.Views
             InitializeComponent();
         }
 
+        private static HttpClient InitializeClient()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:8594");
+            //client.BaseAddress = new Uri("http://localhost:5001");
+            return client;
+        }
+
         private void Button_Clicked(object sender, EventArgs e)
         {
-
+            HttpClient client = InitializeClient();
+            User user = new Models.User { Username = txtUsername.Text, Password = txtPassword.Text };
+            string serializedObject = JsonConvert.SerializeObject(user);
+            var content = new StringContent(serializedObject);
+            content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+            HttpResponseMessage response = client.PutAsync("Login/" + user.Id, content).Result;
         }
     }
 }
